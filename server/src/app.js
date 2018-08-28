5b9e44a3ba5de328ba9c4d4c7d1a57febe4ac2bd
Spider <spider.netpeak@gmail.com>
2018-08-28 15:37:29 +0300

    auth policy added

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const {sequelize} = require('./models');
+const config = require('./config/config');
 const app = express();
 
 app.use(morgan('combined'));
@@ -19,10 +20,12 @@ app.get('/status', function (req, res) {
     )
 });
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.email}! Your user was registered! Have fun!`
-    })
-});
+require('./routes')(app);
 
-app.listen(process.env.PORT || 8081);
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port);
+        console.log(`Server started on port ${config.port}`);
+    })
+
+
